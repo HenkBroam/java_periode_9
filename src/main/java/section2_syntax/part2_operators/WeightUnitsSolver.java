f6429e995db4ab37a22963adfa0d4ed785b8e1c3
Daan <h.a.roorda@st.hanze.nl>
2024-09-16 11:20:22 +0200

    section 2 part 2

--- src/main/java/section2_syntax/part2_operators/WeightUnitsSolver.java
+++ src/main/java/section2_syntax/part2_operators/WeightUnitsSolver.java
@@ -1,30 +1,28 @@
-/*
- * Copyright (c) 2015 Michiel Noback [michiel.noback@gmail.com].
- * All rights reserved.
- */
-
 package section2_syntax.part2_operators;
 
 public class WeightUnitsSolver {
 
     /**
-     * Returns the number of Pounds, Ounces and Grams represented by this quantity of grams,
+     * Returns the number of Pounds, Ounces, and Grams represented by this quantity of grams,
      * encapsulated in a BritishWeightUnits object.
      * @param grams the grams quantity
      * @return a BritishWeightUnits instance
-     * @throws IllegalArgumentException when the Grams quantity is 
+     * @throws IllegalArgumentException when the Grams quantity is less than 0
      */
     public BritishWeightUnits convertFromGrams(int grams) {
-        //YOUR CODE HERE
-        //change this variable to get a correct testing condition
-        final int gramsTest = Integer.MIN_VALUE;
 
-        if (grams <= gramsTest) {
+        if (grams < 0) {
             throw new IllegalArgumentException("Error: grams should be above 0. Given: grams=" + grams);
         }
 
-        //solve the pounds, ounces and grams, create and return a BritishWeightUnits instance
-        //YOUR CODE HERE
-        throw new UnsupportedOperationException("Not implemented yet");
+
+        int pounds = grams / 454;
+        int remainingGramsAfterPounds = grams % 454;
+
+        int ounces = (int) (remainingGramsAfterPounds / 28);
+        int remainingGramsAfterOunces = (int) (remainingGramsAfterPounds % 28);
+
+
+        return new BritishWeightUnits(pounds, ounces, remainingGramsAfterOunces);
     }
 }
