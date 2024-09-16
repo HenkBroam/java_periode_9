435058901d3cb20920bcb76b7c7ad7dadc38eda9
Daan <h.a.roorda@st.hanze.nl>
2024-09-16 12:03:16 +0200

    section 2 part 3

--- src/main/java/section2_syntax/part3_flow_control/AllSubstringsPrinter.java
+++ src/main/java/section2_syntax/part3_flow_control/AllSubstringsPrinter.java
@@ -12,62 +12,69 @@ package section2_syntax.part3_flow_control;
 public class AllSubstringsPrinter {
     /**
      * main method serves development purposes only.
+     *
      * @param args the args
      */
     public static void main(String[] args) {
         final AllSubstringsPrinter asp = new AllSubstringsPrinter();
-        asp.printAllSubstrings("GATCG", true, false); //should print left truncated, left aligned
+        asp.printAllSubstrings("GATCG", true, false);
     }
 
     /**
      * Given a String object, it should print all possible substrings of it where
      * in each iteration the rightmost character is left out.
+     *
      * @param stringToSubstring the string to substring
      */
     public void printSubstringsLeftAlignedLeftTruncated(String stringToSubstring) {
-        //YOUR CODE
+        for (int i = 0; i < stringToSubstring.length(); i++) {
+            String subString = stringToSubstring.substring(i, stringToSubstring.length());
+            System.out.println(subString);
+        }
     }
 
     /**
      * Prints all possible substrings according to arguments. You can make use of {@link #createSpacer(int length) createSpacer} method.
+     *
      * @param stringToSubstring the string to substring
-     * @param leftTruncated flag to indicate whether the substrings should be truncated from the left (or the right)
-     * @param leftAligned flag to indicate whether the substrings should be printed left-aligned (or right-aligned)
+     * @param leftTruncated     flag to indicate whether the substrings should be truncated from the left (or the right)
+     * @param leftAligned       flag to indicate whether the substrings should be printed left-aligned (or right-aligned)
      */
     public void printAllSubstrings(String stringToSubstring, boolean leftTruncated, boolean leftAligned) {
-        //YOUR CODE
+        int length = stringToSubstring.length();
+
+        if (leftTruncated) {
+
+            for (int i = 0; i < length; i++) {
+                String subString = stringToSubstring.substring(i);
+                if (leftAligned) {
+                    System.out.println(subString);
+                } else {
+
+                    System.out.println(createSpacer(length - subString.length()) + subString);
+                }
+            }
+        } else {
+
+            for (int i = 0; i < length; i++) {
+                String subString = stringToSubstring.substring(0, length - i);
+                if (leftAligned) {
+                    System.out.println(subString);
+                } else {
+
+                    System.out.println(createSpacer(i) + subString);
+                }
+            }
+        }
     }
 
     /**
-     * Creates a spacer of given length. This solution uses a StringBuilder which is the correct way to concatenate
-     * Strings in Java.
+     * Creates a string with spaces of the given length.
      *
-     * @param length the spacer length, in number of spaces
-     * @return spacer
+     * @param length the length of the spacer
+     * @return a string containing spaces
      */
-    public String createSpacer(int length) {
-//        String spacer = "";
-        StringBuilder sb = new StringBuilder("");
-        for (int i = 0; i < length; i++) {
-            sb.append(" ");
-//            spacer += " ";
-//            spacer = spacer + " ";
-        }
-//        return spacer;
-        return sb.toString();
-    }
-
-    /**
-     * THIS IS AN EXAMPLE OF HOW NOT TO DO IT!!
-     * String concatenation in for-loops is *evil*
-     * @param length
-     * @return spacer
-     */
-    private String createSpacerTheWrongWay(int length) {
-        String spacer = "";
-        for (int i = 0; i < length; i++) {
-            spacer += " ";
-        }
-        return spacer;
+    private String createSpacer(int length) {
+        return " ".repeat(length);
     }
 }
